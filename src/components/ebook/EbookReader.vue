19d1b35ac8f02044353d006aed201802356aa468
1031167961@qq.com <panyitao@huatu.com>
2021-03-31 23:58:11 +0800

    解析电子书添加注释

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -11,24 +11,30 @@ import Epub from 'epubjs'
 global.epub = Epub
 export default {
   computed: {
+    // 通过vuex获取路径解析文件名
     ...mapGetters(['fileName'])
   },
   methods: {
     initEpub() {
+      // 拼接nginx服务器路径，获取电子书文件
       const url = 'http://localhost:9001/epub/' + this.fileName + '.epub'
       console.log(url)
+      // 实例化电子书
       this.book = new Epub(url)
       console.log(this.book)
       this.rendition = this.book.renderTo('read', {
         width: window.innerWidth,
         height: window.innerHeight,
+        // 适配微信
         methods: 'default'
       })
       this.rendition.display()
     }
   },
   mounted() {
+    // 获取url中的文件路径
     const fileName = this.$route.params.fileName.split('|').join('/')
+    // 修改vuex中的fileName
     this.$store.dispatch('setFileName', fileName).then(() => {
       this.initEpub()
     })
